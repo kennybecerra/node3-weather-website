02216d09586106884967987c299154549d6d8b75
Kenny Becerra <KennyBecerra.j@gmail.com>
2019-05-26 13:49:36 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+// Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setup handlebars engine for views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup statis directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Kenny B'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.send('It works');
+});
+
+app.get('*', (req, res) => {
+  res.send('My 404 Page');
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
